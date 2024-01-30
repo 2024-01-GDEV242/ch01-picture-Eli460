a4fab6b68a2f613ec0311e4b176571379451ae4f
ElishaWhite <123849710+Eli460@users.noreply.github.com>
2024-01-29 23:31:17 -0500

    Final Commit
    Added new hoop, player, goalpost, and changed the positioning of the sun.

--- Picture.java
+++ Picture.java
@@ -19,6 +19,13 @@ public class Picture
     private Square goal1;
     private Square goal2;
     private Circle ball;
+    private Square goal3;
+    private Square goal4;
+    private Square goalPost;
+    private Square goalPost2;
+    private Square goalPost3;
+    private Square goalPost4;
+    private Triangle rim2;
 
     /**
      * Constructor for objects of class Picture
@@ -29,11 +36,18 @@ public class Picture
         line1 = new Square();
         goal1 = new Square();
         goal2 = new Square();
+        goal3 = new Square();
+        goal4 = new Square();
         rim = new Triangle();  
+        rim2 = new Triangle(); 
         sun = new Circle();
         eli = new Person();
         opponent = new Person();
         ball = new Circle();
+        goalPost = new Square();
+        goalPost2 = new Square();
+        goalPost3 = new Square();
+        goalPost4 = new Square();
         drawn = false;
     }
 
@@ -56,44 +70,87 @@ public class Picture
             line1.makeVisible();
     
             eli.changeSize(80,40);
-            eli.moveHorizontal(100);
+            eli.moveHorizontal(-25);
             eli.moveVertical(25);
             eli.makeVisible();
             
             opponent.changeSize(80,40);
-            opponent.moveHorizontal(-50);
+            opponent.moveHorizontal(-150);
             opponent.moveVertical(25);
             opponent.makeVisible();
             
     
             goal1.changeColor("white");
-            goal1.moveHorizontal(-250);
+            goal1.moveHorizontal(-310);
             goal1.moveVertical(-55);
             goal1.changeSize(80);
             goal1.makeVisible();
         
             goal2.changeColor("black");
-            goal2.moveHorizontal(-230);
+            goal2.moveHorizontal(-290);
             goal2.moveVertical(-35);
             goal2.changeSize(35);
             goal2.makeVisible();
             
             rim.changeColor("orange");
             rim.changeSize(20, 30);
-            rim.moveHorizontal(-65);
+            rim.moveHorizontal(-125);
             rim.moveVertical(-15);
             rim.makeVisible();
+            
+            goal3.changeColor("white");
+            goal3.moveHorizontal(110);
+            goal3.moveVertical(-55);
+            goal3.changeSize(80);
+            goal3.makeVisible();
+        
+            goal4.changeColor("black");
+            goal4.moveHorizontal(135);
+            goal4.moveVertical(-35);
+            goal4.changeSize(35);
+            goal4.makeVisible();
+            
+            goalPost.changeColor("black");
+            goalPost.moveHorizontal(-310);
+            goalPost.moveVertical(26);
+            goalPost.changeSize(40);
+            goalPost.makeVisible();
+            
+            goalPost2.changeColor("black");
+            goalPost2.moveHorizontal(-310);
+            goalPost2.moveVertical(60);
+            goalPost2.changeSize(40);
+            goalPost2.makeVisible();
+            
+            goalPost3.changeColor("black");
+            goalPost3.moveHorizontal(150);
+            goalPost3.moveVertical(26);
+            goalPost3.changeSize(40);
+            goalPost3.makeVisible();
+            
+            goalPost4.changeColor("black");
+            goalPost4.moveHorizontal(150);
+            goalPost4.moveVertical(60);
+            goalPost4.changeSize(40);
+            goalPost4.makeVisible();
+        
+            
+            rim2.changeColor("orange");
+            rim2.changeSize(20, 30);
+            rim2.moveHorizontal(200);
+            rim2.moveVertical(-15);
+            rim2.makeVisible();
         
             ball.changeColor("orange");
-            ball.moveHorizontal(15);
-            ball.moveVertical(-10);
+            ball.moveHorizontal(-50);
+            ball.moveVertical(10);
             ball.changeSize(18);
             ball.makeVisible();
         
     
             sun.changeColor("yellow");
-            sun.moveHorizontal(175);
-            sun.moveVertical(-40);
+            sun.moveHorizontal(55);
+            sun.moveVertical(-120);
             sun.changeSize(80);
             sun.makeVisible();
             drawn = true;
@@ -111,7 +168,14 @@ public class Picture
          opponent.changeColor("white");
          goal1.changeColor("white");
          goal2.changeColor("black");
+         goal3.changeColor("white");
+         goal4.changeColor("black");
+         goalPost.changeColor("white");
+         goalPost2.changeColor("white");
+         goalPost3.changeColor("white");
+         goalPost4.changeColor("white");
          rim.changeColor("white");
+         rim2.changeColor("white");
          ball.changeColor("white");
          sun.changeColor("white");
     }
@@ -127,7 +191,14 @@ public class Picture
          opponent.changeColor("yellow");
          goal1.changeColor("white");
          goal2.changeColor("black");
+         goal3.changeColor("white");
+         goal4.changeColor("black");
+         goalPost.changeColor("black");
+         goalPost2.changeColor("black");
+         goalPost3.changeColor("black");
+         goalPost4.changeColor("black");
          rim.changeColor("white");
+         rim2.changeColor("white");
          ball.changeColor("white");
          sun.changeColor("blue");
     }
